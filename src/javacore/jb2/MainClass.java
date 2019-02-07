c4e5209a0c10ebedfb5e9933bfb7e0165eaf2529
Lianna1990 <egoian.lianna@mail.ru>
2019-02-08 01:21:36 +0300

    Надеюсь удалось правильно загрузить домашнее задание

---
+++ src/javacore/jb2/MainClass.java
@@ -0,0 +1,12 @@
+package javacore.jb2;
+
+public class MainClass {
+    public static void main(String[] args) {
+
+    }
+    public void runOneThread() {
+    }
+
+    public void runTwoThreads() {
+    }
+}
